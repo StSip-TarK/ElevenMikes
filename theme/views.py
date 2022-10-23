b63b39c2ce53912f472491596f02b5f066d4c7ca
mart <makaa@ttu.ee>
2022-10-23 20:18:03 +0300

    ADD some starer basic styling

--- theme/views.py
+++ theme/views.py
@@ -11,19 +11,19 @@ import datetime
 
 def index(request):
     start_of_service = datetime.date.fromisoformat("2022-07-18")
+    end_of_service = datetime.date.fromisoformat("2023-06-18")
     now = datetime.date.today()
     time_served = now - start_of_service
-    end_of_service = datetime.date.fromisoformat("2023-06-18")
-    days_left = end_of_service - now
+    days_left = (end_of_service - now).days
     percent = time_served / (end_of_service - start_of_service)
-    html = f'<p>It is {now}</p>'
-    html += f'<p>You started mil service on {start_of_service}</p>'
-    html += f'<p>You have been serving the damn country for {time_served.days} days</p>'
-    html += f'<p>Your service ends on {end_of_service}</p>'
-    html += f'<p>That leaves {days_left.days} days of service left</p>'
-    html += f'<p>You are {percent * 100:.02f}% done with this shithole</p>'
-    context = {
 
+    context = {
+        "start": start_of_service,
+        "now": now,
+        "served": time_served.days,
+        "end": end_of_service,
+        "left": days_left,
+        "percent": f"{percent * 100:.02f}",
     }
 
-    return HttpResponse(html)
+    return render(request, 'index.html', context)
